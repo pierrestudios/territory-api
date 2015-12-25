adfb94e23f02ab5b01b12a3a3a8c32f8e7cd6056
Pierre Studios <info@pierrestudios.com>
2015-12-25 13:15:06 +0000

    installed JWTAuth

--- config/app.php
+++ config/app.php
@@ -157,6 +157,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+		/*
+         * JSON Web Token Authentication...
+         */
+		Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+
     ],
 
     /*
@@ -202,6 +207,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+		'JWTAuth' 	=> Tymon\JWTAuth\Facades\JWTAuth::class,
 
     ],
 
