b6e999d3041391ea824204c468d281c5bbda375f
Pierre Studios <info@pierrestudios.com>
2017-02-10 21:57:52 -0500

    New templates for 404, emails, and lang.json

--- resources/views/translation-en/emails/notice.blade.php
+++ resources/views/translation-en/emails/notice.blade.php
@@ -1,8 +1,11 @@
-<div class="heading">
-	{{$subject}}
-</div>
-<div class="body">
-    {{$content}}
-</div>
-
- 
+<html>
+<head></head>
+<body style="margin: 0 auto">
+	<div style="background: #337ab7; color: #fff; padding: 20px">
+		<h3>{{$subject}}</h3>
+	</div>
+	<div style="color: #333; padding: 20px">
+		<p>{{$content}}</p>
+	</div>
+</body>
+</html>
