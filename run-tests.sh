c9ddbe34a0f1b302c23a6e5d4ccb7483fc1e85c9
Pierre Studios <info@pierrestudios.com>
2018-10-01 15:13:25 -0400

    Make DB refresh by default

--- run-tests.sh
+++ run-tests.sh
@@ -13,23 +13,17 @@ function refresh_db() {
 	log_output "DB refreshed"
 }
 
-# BASH_ARGV
-flags=""
+log_output "Refresh DB"; 
+refresh_db
+
+flags=" --log-json $report_file"
 for a in ${BASH_ARGV[*]} ; do
 	# echo Flag: "$a "
-	if [[ $a == *"--refresh"* ]]
-		then 
-			log_output "Need to refresh DB"; 
-			refresh_db
-	fi
 	if [[ $a == *"--nostyle"* ]]
 		then 
 			flags="$flags --colors=never"
 			# log_output "Flag is set --colors=never";
 	fi
 done
-
-# echo Final Flags: ${flags}
-
 
 ./vendor/bin/phpunit ${flags}
