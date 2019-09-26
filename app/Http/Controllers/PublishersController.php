1899ca00d5c3427f1b4de69e98650c7d3ad2f92d
pierrestudios <info@pierrestudios.com>
2019-09-26 11:49:22 -0400

    Clean up Publishers controller

--- app/Http/Controllers/PublishersController.php
+++ app/Http/Controllers/PublishersController.php
@@ -13,186 +13,198 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 
 class PublishersController extends ApiController
 {
-	public function index(Request $request)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+    public function index(Request $request)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-		if (Gate::denies('view-publishers')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
-		return ['data' => $this->transformCollection(Publisher::latest()->with('territories')->get(), 'publisher')];
-	}
+        if (Gate::denies('view-publishers')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-	public function filter(Request $request)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        return ['data' => $this->transformCollection(Publisher::latest()->with('territories')->get(), 'publisher')];
+    }
 
-		if (Gate::denies('view-publishers')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
-		return ['data' => $this->transformCollection(Publisher::latest()->where(Publisher::applyFilters($request->all()))->with('territories')->get(), 'publisher')];
-	}
+    public function filter(Request $request)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-	public function users(Request $request)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        if (Gate::denies('view-publishers')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-		if (Gate::denies('admin')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
-		// dd(User::latest()->with('publisher.territories')->get());
+        return [
+            'data' => $this->transformCollection(
+                Publisher::latest()->where(
+                    Publisher::applyFilters($request->all())
+                )->with('territories')->get(),
+                'publisher'
+            )
+        ];
+    }
 
-		return ['data' => $this->transformCollection(User::latest()->with('publisher.territories')->get(), 'user')];
-	}
+    public function users(Request $request)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-	public function saveUser(Request $request, $userId)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        if (Gate::denies('admin')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-		if (Gate::denies('admin')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
+        return ['data' => $this->transformCollection(User::latest()->with('publisher.territories')->get(), 'user')];
+    }
 
-		try {
-			$user = User::findOrFail($userId);
-			$user->update(["email" => $request->input('email'), "level" => User::getType($request->input('userType'))]);
-		} catch (Exception $e) {
-			$data = ['error' => 'Publisher not found', 'message' => $e->getMessage()];
-		}
-		// dd($user);
+    public function saveUser(Request $request, $userId)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-		return ['data' => $user ? true : null];
-	}
+        if (Gate::denies('admin')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-	public function attachUser(Request $request)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        try {
+            $user = User::findOrFail($userId);
+            $user->update(
+                [
+                    "email" => $request->input('email'),
+                    "level" => User::getType($request->input('userType'))
+                ]
+            );
+        } catch (Exception $e) {
+            return ['data' => null, 'error' => 'Publisher not found', 'message' => $e->getMessage()];
+        }
 
-		if (Gate::denies('admin')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
+        return ['data' => $user ? true : null];
+    }
 
-		try {
-			$publisher = Publisher::findOrFail($request->input('publisherId'));
-			$publisher->user_id = $request->input('userId');
-			$publisher->save();
-		} catch (Exception $e) {
-			$data = ['error' => 'Publisher not found', 'message' => $e->getMessage()];
-		}
-		// dd($publisher);
+    public function attachUser(Request $request)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-		return ['data' => $publisher ? true : null];
-	}
+        if (Gate::denies('admin')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-	public function deleteUser(Request $request, $userId)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        try {
+            $publisher = Publisher::findOrFail($request->input('publisherId'));
+            $publisher->user_id = $request->input('userId');
+            $publisher->save();
+        } catch (Exception $e) {
+            return ['data' => null, 'error' => 'Publisher not found', 'message' => $e->getMessage()];
+        }
 
-		if (Gate::denies('admin')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
+        return ['data' => $publisher ? true : null];
+    }
 
-		try {
-			$user = User::findOrFail($userId);
-			// dd($user);
-			$data = $user->delete();
-		} catch (Exception $e) {
-			$data = ['error' => 'User not found', 'message' => $e->getMessage()];
-		}
-		// dd($user);
+    public function deleteUser(Request $request, $userId)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-		return ['data' => $user ? true : null];
-	}
+        if (Gate::denies('admin')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-	public function view(Request $request, $publisherId = null)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        try {
+            $user = User::findOrFail($userId);
+            $data = $user->delete();
+        } catch (Exception $e) {
+            return ['data' => null, 'error' => 'User not found', 'message' => $e->getMessage()];
+        }
 
-		if (Gate::denies('view-publishers')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
+        return ['data' => $user ? true : null];
+    }
 
-		try {
-			$publisher = Publisher::where('id', $publisherId)->with('territories')->get();
-			$data = !empty($publisher[0]) ? $this->transform($publisher[0]->toArray(), 'publisher') : null;
-		} catch (Exception $e) {
-			$data = ['error' => 'Publisher not found', 'message' => $e->getMessage()];
-		}
-		return ['data' => $data];
-	}
+    public function view(Request $request, $publisherId = null)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-	public function save(Request $request, $publisherId = null)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        if (Gate::denies('view-publishers')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-		if (Gate::denies('update-publishers')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
+        try {
+            $publisher = Publisher::where('id', $publisherId)->with('territories')->get();
+            $data = !empty($publisher[0]) ? $this->transform($publisher[0]->toArray(), 'publisher') : null;
+        } catch (Exception $e) {
+            return ['data' => null, 'error' => 'Publisher not found', 'message' => $e->getMessage()];
+        }
 
-		if (!empty($publisherId)) {
-			try {
-				$publisher = Publisher::findOrFail($publisherId);
-				$publisher->update(["first_name" => $request->input('firstName'), "last_name" => $request->input('lastName')]);
-				$data = !empty($publisher) ? $this->transform($publisher->toArray(), 'publisher') : null;
-			} catch (Exception $e) {
-				$data = ['error' => 'Publisher not found', 'message' => $e->getMessage()];
-			}
-		}
-		return ['data' => $data];
-	}
+        return ['data' => $data];
+    }
 
-	public function add(Request $request)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+    public function save(Request $request, $publisherId = null)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-		if (Gate::denies('update-publishers')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
+        if (Gate::denies('update-publishers')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-		try {
-			$publisher = Publisher::create(["first_name" => $request->input('firstName'), "last_name" => $request->input('lastName')]);
-			$data = !empty($publisher) ? $this->transform($publisher->toArray(), 'publisher') : null;
-		} catch (Exception $e) {
-			$data = ['error' => 'Publisher not created', 'message' => $e->getMessage()];
-		}
-		return ['data' => $data];
-	}
+        if (!empty($publisherId)) {
+            try {
+                $publisher = Publisher::findOrFail($publisherId);
+                $publisher->update(["first_name" => $request->input('firstName'), "last_name" => $request->input('lastName')]);
+                $data = !empty($publisher) ? $this->transform($publisher->toArray(), 'publisher') : null;
+            } catch (Exception $e) {
+                return ['data' => null, 'error' => 'Publisher not found', 'message' => $e->getMessage()];
+            }
+        }
 
-	public function delete(Request $request, $publisherId)
-	{
-		if (!$this->hasAccess($request)) {
-			return Response()->json(['error' => 'Access denied.'], 500);
-		}
+        return ['data' => $data];
+    }
 
-		if (Gate::denies('admin')) {
-			return Response()->json(['error' => 'Method not allowed'], 403);
-		}
+    public function add(Request $request)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
 
-		try {
-			$publisher = Publisher::findOrFail($publisherId);
-			$data = $publisher->delete();
-		} catch (Exception $e) {
-			$data = ['error' => 'User not found', 'message' => $e->getMessage()];
-		}
+        if (Gate::denies('update-publishers')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
 
-		return ['data' => $publisher ? true : null];
-	}
+        try {
+            $publisher = Publisher::create(["first_name" => $request->input('firstName'), "last_name" => $request->input('lastName')]);
+            $data = !empty($publisher) ? $this->transform($publisher->toArray(), 'publisher') : null;
+        } catch (Exception $e) {
+            return ['data' => null, 'error' => 'Publisher not created', 'message' => $e->getMessage()];
+        }
+
+        return ['data' => $data];
+    }
+
+    public function delete(Request $request, $publisherId)
+    {
+        if (!$this->hasAccess($request)) {
+            return Response()->json(['error' => 'Access denied.'], 500);
+        }
+
+        if (Gate::denies('admin')) {
+            return Response()->json(['error' => 'Method not allowed'], 403);
+        }
+
+        try {
+            $publisher = Publisher::findOrFail($publisherId);
+            $data = $publisher->delete();
+        } catch (Exception $e) {
+            return ['data' => null, 'error' => 'User not found', 'message' => $e->getMessage()];
+        }
+
+        return ['data' => $publisher ? true : null];
+    }
 }
