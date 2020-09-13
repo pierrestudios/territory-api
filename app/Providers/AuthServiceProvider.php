95e64d7d038d327c09b5593fd689680e6575afcb
Pierre Studios <info@pierrestudios.com>
2020-09-12 23:12:47 -0400

    Fixed auth Gate setup

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,10 +21,88 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(GateContract $gate)
     {
         $this->registerPolicies();
 
-        //
+        $gate->before(
+            function ($user, $ability) {
+                if ($user->isAdmin()) {
+                    return true;
+                }
+            }
+        );
+
+        $gate->define(
+            'admin', function ($user) {
+                return $user->isAdmin();
+            }
+        );
+        
+        $gate->define(
+            'view-publishers', function ($user) {
+                return $user->isManager();
+            }
+        );
+        
+        $gate->define(
+            'update-publishers', function ($user) {
+                return $user->isManager();
+            }
+        );
+        
+        $gate->define(
+            'delete-publishers', function ($user) {
+                return $user->isAdmin();
+            }
+        );
+        
+        $gate->define(
+            'update-territories', function ($user) {
+                return $user->isManager();
+            }
+        );
+        
+        $gate->define(
+            'delete-teritorries', function ($user) {
+                return $user->isAdmin();
+            }
+        );
+        
+        $gate->define(
+            'create-addresses', function ($user) {
+                return $user->isEditor();
+            }
+        );
+        
+        $gate->define(
+            'update-addresses', function ($user) {
+                return $user->isEditor();
+            }
+        );
+        
+        $gate->define(
+            'delete-addresses', function ($user) {
+                return $user->isAdmin();
+            }
+        );
+        
+        $gate->define(
+            'soft-delete-addresses', function ($user) {
+                return $user->isEditor();
+            }
+        );
+        
+        $gate->define(
+            'create-notes', function ($user) {
+                return $user->isNoteEditor();
+            }
+        );
+        
+        $gate->define(
+            'update-notes', function ($user, $note) {
+                return $user->isOwner($note);
+            }
+        );
     }
 }
