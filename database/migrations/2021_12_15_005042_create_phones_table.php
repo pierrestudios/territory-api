8afbe5dbe5b6c9310ab89ea9dfa135c9ba7fb776
Pierre Studios <info@pierrestudios.com>
2021-12-18 19:54:15 -0500

    TERRAPI-23 - migration for Phone

---
+++ database/migrations/2021_12_15_005042_create_phones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->integer('address_id');
+            $table->string('name')->nullable();
+            $table->string('number')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->unique(array('number'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
