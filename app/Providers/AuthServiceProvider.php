2c45b78245575611e13f9feebfeed4f02f62e783
Pierre Studios <info@pierrestudios.com>
2021-12-18 19:52:24 -0500

    TERRAPI-24 - Added phones entry for Auth Gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -38,70 +38,82 @@ class AuthServiceProvider extends ServiceProvider
                 return $user->isAdmin();
             }
         );
-        
+
         $gate->define(
             'view-publishers', function ($user) {
                 return $user->isManager();
             }
         );
-        
+
         $gate->define(
             'update-publishers', function ($user) {
                 return $user->isManager();
             }
         );
-        
+
         $gate->define(
             'delete-publishers', function ($user) {
                 return $user->isAdmin();
             }
         );
-        
+
         $gate->define(
             'update-territories', function ($user) {
                 return $user->isManager();
             }
         );
-        
+
         $gate->define(
             'delete-teritorries', function ($user) {
                 return $user->isAdmin();
             }
         );
-        
+
         $gate->define(
             'create-addresses', function ($user) {
                 return $user->isEditor();
             }
         );
-        
+
         $gate->define(
             'update-addresses', function ($user) {
                 return $user->isEditor();
             }
         );
-        
+
         $gate->define(
             'delete-addresses', function ($user) {
                 return $user->isAdmin();
             }
         );
-        
+
         $gate->define(
             'soft-delete-addresses', function ($user) {
                 return $user->isEditor();
             }
         );
-        
+
         $gate->define(
             'create-notes', function ($user) {
                 return $user->isNoteEditor();
             }
         );
-        
+
         $gate->define(
             'update-notes', function ($user, $note) {
                 return $user->isOwner($note);
+            }
+        );
+
+        $gate->define(
+            'create-phones', function ($user) {
+                return $user->isNoteEditor();
+            }
+        );
+
+        $gate->define(
+            'update-phones', function ($user, $phone) {
+                return $user->isOwner($phone);
             }
         );
     }
